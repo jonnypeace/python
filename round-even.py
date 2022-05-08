68f60b010b621e01771c7eb0deaa75ea9afc2668
Jonny <royalpython2@hotmail.com>
2022-05-08 18:15:02 +0100

    Round values even
    Well, i've done it with gawk, bash and microsoft excel, so might as well include a python version

---
+++ round-even.py
@@ -0,0 +1,45 @@
+def round_val(x, r):
+    # looking for the last to characters for conditional statements.
+    z = str(int(x * (10 ** (r + 1))))
+    q = z[-2:]
+
+    # finding index position of decimal point
+    point = str(x).index(".")
+
+    # finding length of decimal number as a string
+    length = len(str(x))
+
+    # multiplying x into an integer value, since modulus doesn't work with floating numbers very well.
+    mod1 = x * (10 ** (length - (point + 1)))
+
+    # result of modulus into 5 needs to be zero for rounding even to occur.
+    mod2 = mod1 % 5
+
+    # checking conditions are met for rounding even
+    if q == "05" or q == "25" or q == "45" or q == "65" or q == "85" and mod2 == 0.0:
+        ans = round((mod1 - 5) / 10 ** (length - (point + 1)), r)
+        # this syntax works along the lines of "{:.2f}".format(val, decimal_point). .2f would be 2 decimal points
+        # i.e. "{:.decimal_pointf}".format(val, decimal_point)
+        # for some more info https://mkaz.blog/code/python-string-format-cookbook/
+        ans = "{:.{}f}".format(ans, r)
+        print("Answer is: " + ans)
+        return
+    elif q == "15" or q == "35" or q == "55" or q == "75" or q == "95" and mod2 == 0.0:
+        ans = (mod1 + 5) / 10 ** (length - (point + 1))
+        ans = "{:.{}f}".format(ans, r)
+        print("Answer is: " + ans)
+        return
+    else:
+        ans = "{:.{}f}".format(x, r)
+        print("Answer is: " + ans)
+        return
+
+
+# forever loop while run = y
+# calls the round_val function and passes in the parameters x and r.
+run = "y"
+while run == "y":
+    x = float(input('Decimal number for rounding: '))
+    r = int(input('Number of decimal points: '))
+    round_val(x, r)
+    run = input("Do you want to continue? Type 'y' if you do: ")
