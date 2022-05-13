64f6ae9451378fc3089aa063a825c7e6842ce1bd
Jonny <royalpython2@hotmail.com>
2022-05-13 19:48:54 +0100

    Update round-even.py

--- round-even.py
+++ round-even.py
@@ -2,48 +2,54 @@
 
 import re
 
-def round_val(x, r):
-    # looking for the last to characters for conditional statements.
-    z = str(int(x * (10 ** (r + 1))))
-    q = z[-2:]
 
+def round_val(x, r):
     # finding index position of decimal point
     point = str(x).index(".")
 
     # finding length of decimal number as a string
     length = len(str(x))
 
-    # multiplying x into an integer value, since modulus doesn't work with floating numbers very well.
-    mod1 = x * (10 ** (length - (point + 1)))
+    # left of the decimal
+    left_d = str(x)[:point]
+
+    # right of the decimal
+    right_d = str(x)[point + 1:]
+
+    # concatenate left and right of the decimal (avoid floating math problems)
+    l_and_r = (left_d + right_d)
+
+    # looking for the last to characters for conditional statements.
+    last_two = str(l_and_r)[-2:]
 
     # result of modulus into 5 needs to be zero for rounding even to occur.
-    mod2 = mod1 % 5
+    mod1 = float(l_and_r) % 5
 
     # checking conditions are met for rounding even
-    if re.findall("^[02468]?5$", q) and mod2 == 0.0:
-        ans = (mod1 - 5) / 10 ** (length - (point + 1))
+    if re.findall("^[02468]?5$", last_two) and mod1 == 0.0:
+        ans = round((float(l_and_r) - 5) / 10 ** (length - (point + 1)), r)
         # this syntax works along the lines of "{:.2f}".format(val, decimal_point). .2f would be 2 decimal points
         # i.e. "{:.decimal_pointf}".format(val, decimal_point)
         # for some more info https://mkaz.blog/code/python-string-format-cookbook/
         ans = "{:.{}f}".format(ans, r)
-        print("Answer is: " + ans)
-        return
-    elif re.findall("^[13579]5$", q) and mod2 == 0.0:
-        ans = (mod1 + 5) / 10 ** (length - (point + 1))
+        return ans
+    elif re.findall("^[13579]5$", last_two) and mod1 == 0.0:
+        ans = (float(l_and_r) + 5) / 10 ** (length - (point + 1))
         ans = "{:.{}f}".format(ans, r)
-        print("Answer is: " + ans)
-        return
+        return ans
     else:
         ans = "{:.{}f}".format(x, r)
-        print("Answer is: " + ans)
-        return
+        return ans
 
 
 # forever loop while run = y
 # calls the round_val function and passes in the parameters x and r.
+# While loop used for manual testing.
 run = "y"
 while run == "y":
     x = float(input('Decimal number for rounding: '))
     r = int(input('Number of decimal points: '))
-    round_val(x, r)
+    calc = round_val(x, r)
+    print("Answer is: " + str(calc))
     run = input("Do you want to continue? Type 'y' if you do: ")
+
